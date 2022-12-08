910d42e0b79c671fa7ac6663540cd6c62977d98c
simsang1l <simsangil1db@gmail.com>
2022-12-09 08:34:44 +0900

    2022.12.08 hackerrank

---
+++ Python/Between_Two_Sets.py
@@ -0,0 +1,20 @@
+import math
+import os
+import random
+import re
+import sys
+from math import gcd
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+# 첫번째 배열의 공배수들
+# 두번째 배열의 공약수들
+# 두개 배열에서 공통되는 원소의 수
+
---
+++ SQL/Weather_Observation_Station_12.sql
@@ -0,0 +1,8 @@
+select distinct
+    CITY
+from
+    STATION
+where
+    left(CITY, 1) not in ('A', 'E', 'I', 'O', 'U')
+    and right(CITY, 1) not in ('a', 'e', 'i', 'o', 'u')
+;
