9ed1db25c98bde3cc22a9b16c78a0985e3a23c6e
simsang1l <simsang1db@gmail.com>
2022-12-13 09:54:23 +0900

    2022.12.13 hackerrank

--- Python/Between_Two_Sets.py
+++ Python/Between_Two_Sets.py
@@ -4,6 +4,7 @@ import random
 import re
 import sys
 from math import gcd
+from functools import reduce
 
 #
 # Complete the 'getTotalX' function below.
@@ -18,3 +19,16 @@ from math import gcd
 # 두번째 배열의 공약수들
 # 두개 배열에서 공통되는 원소의 수
 
+def getTotalX(a, b):
+    # Write your code here
+    lcm = reduce(lambda x, y : x*y // gcd(x, y), a) # 첫번째 array의 최소공배수
+    cd = reduce(lambda x, y, : gcd(x, y), b) # 두번째 array의 최대공약수
+    
+    cnt = 0 
+    # 1에서 최대공약수 까지 반복
+    for i in range(1, cd//lcm + 1) :
+        # 최대공약수가 최소공배수 * i를 했을 때 0이라면 정답개수 1씩 증가
+        if cd % (lcm * i) == 0: 
+            cnt += 1
+            
+    return cnt
---
+++ SQL/Employee_Names.sql
@@ -0,0 +1,8 @@
+-- alphabetical order ...만 고려하면 된다
+select
+    name
+from
+    Employee
+order by
+    name
+;
